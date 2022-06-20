60af650d840a72d17c41e2f85204c1a5471e4541
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-06-20 20:27:04 -0300

    :bug: fix: Corrigindo requisito 3 - estava com ids trocados

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,5 @@
-SELECT name_user AS usuario, COUNT(*) AS qtde_musicas_ouvidas, ROUND(SUM(msc.id_song) / 60, 2) AS total_minutos FROM SpotifyClone.user AS us
+SELECT us.name_user AS usuario, COUNT(msc.id_song) AS qtde_musicas_ouvidas, ROUND(SUM((msc.duration_seconds) / 60), 2) AS total_minutos FROM SpotifyClone.user AS us
 JOIN SpotifyClone.history_plays AS h_plays ON us.id_user = h_plays.id_user
-JOIN SpotifyClone.songs AS msc ON h_plays.id_history = msc.id_song
+JOIN SpotifyClone.songs AS msc ON h_plays.id_song = msc.id_song
 GROUP BY us.name_user
 ORDER BY us.name_user;
