a083872f957d77a7ef8df26c049c435fcf9a3399
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-06-20 21:51:49 -0300

    :sparkles: feat: Adicionando requisito 7 - :clown_face:

---
+++ desafio7.sql
@@ -0,0 +1,16 @@
+-- USE SpotifyClone;
+
+-- SELECT * FROM SpotifyClone.user;
+-- SELECT * FROM SpotifyClone.history_plays;
+-- SELECT * FROM SpotifyClone.songs;
+-- SELECT * FROM SpotifyClone.following_artists;
+-- SELECT * FROM SpotifyClone.albums;
+-- SELECT * FROM SpotifyClone.artists;
+-- SELECT * FROM SpotifyClone.plano;
+
+SELECT ART.artist_name AS artista, ALB.album_name AS album, COUNT(FA.id_artist) AS seguidores
+FROM SpotifyClone.artists AS ART
+JOIN SpotifyClone.albums AS ALB ON ART.id_artist = ALB.id_artist
+JOIN SpotifyClone.following_artists AS FA ON ART.id_artist = FA.id_artist
+GROUP BY ART.artist_name, ALB.album_name
+ORDER BY seguidores DESC;
