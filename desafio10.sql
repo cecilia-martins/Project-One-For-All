15b19f5a3913ea17c5fbbb5d9c0b07534ddcc909
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-06-20 22:41:09 -0300

    :bug: fix: Corrigindo requisito 10 - na verdade, completando.. adicionei a parte dos planos

--- desafio10.sql
+++ desafio10.sql
@@ -11,5 +11,8 @@
 SELECT MSC.song_name AS nome, COUNT(HP.id_song) AS reproducoes
 FROM SpotifyClone.songs AS MSC
 JOIN SpotifyClone.history_plays AS HP ON MSC.id_song = HP.id_song
+JOIN SpotifyClone.user AS US ON HP.id_user = US.id_user
+JOIN SpotifyClone.plano AS P ON P.id_plano = US.id_plano
+WHERE plano_type = 'gratuito' OR plano_type = 'pessoal'
 GROUP BY MSC.song_name
 ORDER BY MSC.song_name ASC;
